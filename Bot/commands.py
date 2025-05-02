1b68cf3ccfdc6fbc6e1040b5c7a9f774057f419e
Mike Schubert <mike.schubert96@outlook.de>
2025-05-02 20:57:30 +0200

    restructure discord bot and main structure

---
+++ Bot/commands.py
@@ -0,0 +1,31 @@
+﻿from discord.ext.commands import Context
+from Bot.discordBot import ServerBot
+
+import server
+
+
+def __init__(client: ServerBot) -> None:
+    @client.command()
+    async def hello(ctx: Context) -> None:
+        await ctx.channel.send(f'hello there {ctx.author.mention}')
+
+    @client.command()
+    async def clear(ctx: Context) -> None:
+        await ctx.channel.delete()
+        new_channel = await ctx.channel.clone(reason="Channel was purged")
+        await new_channel.edit(position=ctx.channel.position)
+        await new_channel.send("Channel was purged")
+
+    @client.command()
+    async def start(ctx: Context, args) -> None:
+        try:
+            server.start_application(args)
+            await ctx.channel.send(server.onstarted(args))
+        except FileNotFoundError:
+            await ctx.channel.send("There is no such server")
+
+
+    @client.command()
+    async def status(ctx: Context, args) -> None:
+        statusMessage = server.check_status(args)
+        await ctx.channel.send(statusMessage)
---
+++ Bot/discordBot.py
@@ -0,0 +1,24 @@
+﻿import discord
+from discord.ext import commands
+
+
+class ServerBot(commands.Bot):
+
+    async def on_ready(self) -> None:
+        print(f"{self.user.name} is ready")
+
+    async def on_message(self, message) -> None:
+        if message.author == self.user:
+            return
+
+        print(f"Received message:\nAuthor: {message.author}\nContent: {message.content}\nChannel: {message.channel.name}")
+        await self.process_commands(message)
+
+
+def __init__() -> ServerBot:
+    intents = discord.Intents.default()
+    intents.message_content = True  # NOQA
+
+    client: ServerBot = ServerBot(command_prefix='$', intents=intents)
+
+    return client
